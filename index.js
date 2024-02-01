06e7d925920ee1623edf983bd730ee9237877f80
gbabyblue <114004755+gbabyblue@users.noreply.github.com>
2024-02-01 00:31:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1089,7 +1089,7 @@ let csit240Chapter1Vocab = [
   },
   {
     vocab: 'Semantics',
-    definition: 'Rules that deine what a statment in a language means.'
+    definition: 'Rules that define what a statment in a language means.'
   },
   {
     vocab: 'Java 2 Platform',
